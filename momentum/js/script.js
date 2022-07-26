fe7b44592e3bee0fa37ac74c9cdd038b6c9e3da1
Larisa Volynkina <volynkinala@gmail.com>
2022-07-26 15:19:57 +0400

    feat: add weather

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -56,8 +56,95 @@ updateTime();
 
 const input = document.querySelector('.name');
 
+// Slider
+
+function getRandomNum() {
+  return Math.floor(Math.random() * (21 - 1)) + 1;
+}
+let bgNum = getRandomNum();
+
+const slidePrev = document.querySelector('.slide-prev');
+const slideNext = document.querySelector('.slide-next');
+
+function setBg() {
+  const timeOfDayforSlider = getTimeOfDay().slice(5);
+  if (bgNum < 10) {
+    bgNum = `0${bgNum.toString()}`;
+  }
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/larvolynkina/momentum-images/main/images/${timeOfDayforSlider}/${bgNum}.webp`;
+  img.addEventListener('load', () => {
+    document.body.style.backgroundImage = `url(${img.src})`;
+  });
+}
+
+function getSlidePrev() {
+  bgNum = +bgNum - 1;
+  if (bgNum === 0) {
+    bgNum = 20;
+  }
+  setBg();
+}
+
+function getSlideNext() {
+  bgNum = +bgNum + 1;
+  if (bgNum === 21) {
+    bgNum = 1;
+  }
+  setBg();
+}
+
+slidePrev.addEventListener('click', getSlidePrev);
+slideNext.addEventListener('click', getSlideNext);
+
+setBg();
+
+// Weather
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const inputCity = document.querySelector('.city');
+const weatherError = document.querySelector('.weather-error');
+inputCity.value = 'Minsk';
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&lang=en&appid=193b021fd8d76c22e00563035b5f6d2d&units=metric`;
+
+async function getWeather() {
+  const res = await fetch(url);
+  const data = await res.json();
+  if (data.cod === '404' || inputCity.value === '') {
+    weatherDescription.textContent = '';
+    weatherIcon.className = 'weather-icon owf';
+    temperature.textContent = '';
+    wind.textContent = '';
+    humidity.textContent = '';
+    if (data.cod === '404') {
+      weatherError.textContent = `Error! city not found for '${inputCity.value}'!`;
+    } else if (inputCity.value === '') {
+      weatherError.textContent = `Error! Nothing to geocode for '${inputCity.value}'!`;
+    }
+  } else {
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    weatherError.textContent = '';
+    wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  }
+}
+getWeather();
+
+inputCity.addEventListener('change', () => {
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&lang=en&appid=193b021fd8d76c22e00563035b5f6d2d&units=metric`;
+  getWeather();
+});
+
 function setLocalStorage() {
   localStorage.setItem('name', input.value);
+  localStorage.setItem('city', inputCity.value);
 }
 window.addEventListener('beforeunload', setLocalStorage);
 
@@ -65,22 +152,9 @@ function getLocalStorage() {
   if (localStorage.getItem('name')) {
     input.value = localStorage.getItem('name');
   }
+  if (localStorage.getItem('city')) {
+    inputCity.value = localStorage.getItem('city');
+    getWeather();
+  }
 }
 window.addEventListener('load', getLocalStorage);
-
-// Slider
-
-function getRandomNum() {
-  return Math.floor(Math.random() * (21 - 1)) + 1;
-}
-
-function setBg() {
-  const timeOfDayforSlider = getTimeOfDay().slice(5);
-  let bgNum = getRandomNum();
-  if (bgNum < 10) {
-    bgNum = `0${bgNum.toString()}`;
-  }
-  document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/larvolynkina/stage1-tasks/blob/assets/images/${timeOfDayforSlider}/${bgNum}.jpg')`;
-}
-
-setBg();
